bdcf4c9b7e345c8c44de01f580810e9aa169db7d
murali-cse <muralicse97@gmail.com>
2023-01-07 13:32:05 +0530

    validations are fixed

--- application/views/admin_login.php
+++ application/views/admin_login.php
@@ -3,16 +3,18 @@
         <div class="row h-100 justify-content-center align-items-center">
             <div class="col-sm-12 col-xs-12 col-md-6 col-lg-6">
                 <h3>Admin Login</h3>
-                <form action="<?php echo base_url('register/admin_login') ?>" method="post">
+                <form action="<?php echo base_url('register/admin_login') ?>" method="post" id="loginform">
                     <div class="form-floating mb-3 mt-4">
                         <input type="email" class="form-control" id="floatingInput" name="email"
                             placeholder="name@example.com">
                         <label for="floatingInput">Email address</label>
+                        <div id="error_email" class="form-text text-danger text-start"></div>
                     </div>
                     <div class="form-floating mb-3">
                         <input type="password" class="form-control" id="floatingPassword" name="pass"
                             placeholder="*****">
                         <label for="floatingPassword">password</label>
+                        <div id="error_pass" class="form-text text-danger text-start"></div>
                     </div>
                     <div class="alert alert-danger" role="alert"
                         style="display:<?php echo $this->session->flashdata('login_alert')== true ? 'block' : 'none' ?>">
@@ -29,3 +31,29 @@
         </div>
     </div>
 </div>
+
+<script>
+$(function() {
+
+    $('#loginform').on('submit', function(e) {
+        e.preventDefault();
+
+        let email = $('#floatingInput').val();
+        let pass = $('#floatingPassword').val()
+
+        if (email && pass) {
+            this.submit()
+        } else {
+            if (!email) {
+                $('#error_email').text('Please enter a valid email')
+            }
+            if (!pass) {
+                $('#error_pass').text('Please enter a valid password')
+            }
+        }
+
+
+    })
+
+})
+</script>
